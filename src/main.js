d3eaffb71797216d8b438a10b40d26e507d4f51d
unknown <mr.elmertoledo@gmail.com>
2020-10-06 23:44:50 +0800

    File read

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 var http = require("http");
+var fs = require("fs");
 
 http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.write("Hello world.");
-    res.end();
-
+    fs.readFile('index.html',function(err, data){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+        res.end();
+    });
 
 }).listen(8080);
