0b897b64cdb552b27de689390f6415a34bd8fa3e
unknown <mr.elmertoledo@gmail.com>
2020-10-07 01:00:31 +0800

    File Checking

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 var http = require("http");
 var fs = require("fs");
+var url = require("url");
 
 http.createServer(function(req,res){
-    fs.readFile('index.html',function(err, data){
-        res.writeHead(200,{'Content-Type':'text/html'});
-        res.write(data);
-        res.end();
-    });
+    var baseURL = url.parse(req.url,true);
+    var page = "." + baseURL.pathname;
 
+    fs.readFile(page,function(err, data){
+        if(err){
+          res.writeHead(404,{'Content-Type' : 'text/html'});
+          return res.end("404 Not Found");
+
+        }
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+    });
 }).listen(8080);
