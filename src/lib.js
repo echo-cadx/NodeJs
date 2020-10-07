834f7fbe324851b9e5af5c9ed977d8c23b7a1cb7
unknown <mr.elmertoledo@gmail.com>
2020-10-07 22:42:44 +0800

    npm

---
+++ src/lib.js
@@ -0,0 +1,4 @@
+module.exports.http = require("http");
+module.exports.url  = require("url");
+module.exports.fs = require("fs");
+module.exports.string_uc = require("upper-case");
--- src/main.js
+++ src/main.js
@@ -1,19 +1,18 @@
-var http = require("http");
-var fs = require("fs");
-var url = require("url");
+var lib = require("./lib");
 
-http.createServer(function(req,res){
-    var baseURL = url.parse(req.url,true);
-    var page = "." + baseURL.pathname;
+lib.http.createServer(function(req, res){
+    var baseURL = lib.url.parse(req.url,true);
+    var links = "." + baseURL.pathname;
 
-    fs.readFile(page,function(err, data){
+    lib.fs.readFile(links, function(err, data){
         if(err){
-          res.writeHead(404,{'Content-Type' : 'text/html'});
-          return res.end("404 Not Found");
-
+            res.writeHead(404,{"Content-Type":"text/html"});
+            return res.end("404 Not Found!");
         }
-    res.writeHead(200,{'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
+        res.writeHead(200,{"Content-Type":"text/html"});
+        res.write(data);
+        res.write(lib.string_uc.upperCase("cadx"));
+        return res.end();
     });
+
 }).listen(8080);
